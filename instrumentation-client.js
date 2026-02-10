464331c489a71d0315b83561e647e56fe5c398d7
SystemInfomation <forsyth@ahscampus.com>
2026-02-10 15:34:06 -0500

    Update instrumentation-client.js
    Signed-off-by: SystemInfomation <forsyth@ahscampus.com>

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -2,5 +2,5 @@ import posthog from 'posthog-js'
 
 posthog.init('phc_UxFsQxTXGf827YwM7QGrkVyQUhbQ6tTdknX2cfdoCkv', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-    defaults: '2025-11-30'
+    defaults: '2026-10-10'
 });
